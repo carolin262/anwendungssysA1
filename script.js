7e0d8c49752714cddd55d69dda3de1534e0b3a34
TheoVitkovskiy <fvitkovski@mail.de>
2017-05-23 09:26:52 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
+var minNumber = 220;
+var maxNumber = 240;
+
+var datum = new Date();
+var heute = datum.getDate()+ "." + (datum.getMonth()+ 1)+ "." + datum.getFullYear();
+
+var spannung = randomNumberFromRange(minNumber, maxNumber);
+var strom = randomNumberFromRange(0, parseInt(document.getElementById("maxstrom").innerHTML) + 5);
+
+
+function randomNumberFromRange(min,max)
+{
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+//JQuery
 $( document ).ready(function() {
-	var video = document.getElementById('bgVideo');
-    video.playbackRate = 0.5;
+		$("#spannung").html(spannung);
+		$("#strom").html(strom);
+    if(strom > parseInt(document.getElementById("maxstrom").innerHTML)){
+      $("#warnung").css("display", "block");
+    }
+    $("#btnAblesen").click(function(){
+        $('#tableAblesen').append('<tr><td>' + $("input[name=nutzerkennung]").val() + '</td><td>' + $("input[name=verbrauchswert]").val() + '</td><td>' + heute +'</td></tr>');
+    });
+
 });
