edbd79f6a58a3b845af63420891091faecfa3ecd
jiafug <comet.liu@protonmail.com>
2017-05-23 20:46:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,37 @@
-function zufall() {
-    var z = Math.random();
-    z *= 20;
-    z += 220;
-    z = Math.floor(z);
-    var spannung = document.getElementById("spannung");
-    spannung.textContent = "aktuelle Spannung: " + z + " U";
-    z = Math.random();
-    z *= 55;
-    z += 1;
-    z = Math.floor(z);
-    var strom = document.getElementById("strom");
-    if (z > 50) {
-        strom.textContent = "aktueller Strom: " + z + " A" + "   ACHTUNG ! Die maximale Belastung ist überkrass!";
-    } else {
-        strom.textContent = "aktueller Strom: " + z + " A";
+var minNumber = 220;
+var maxNumber = 240;
+
+var datum = new Date();
+var heute = datum.getDate()+ "." + (datum.getMonth()+ 1)+ "." + datum.getFullYear();
+
+var spannung = randomNumberFromRange(minNumber, maxNumber);
+var strom = randomNumberFromRange(0, parseInt(document.getElementById("maxstrom").innerHTML) + 5);
+
+
+function randomNumberFromRange(min,max)
+{
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+function validateInput(val) {
+    return /[A-Z]{2}\d{2}/i.test(val);
+}
+
+//JQuery
+$( document ).ready(function() {
+		$("#spannung").html(spannung);
+		$("#strom").html(strom);
+    if(strom > parseInt(document.getElementById("maxstrom").innerHTML)){
+      $("#warnung").css("display", "block");
     }
-}
-function ablesen() {
-    var d = new Date();
-    var datum = d.toISOString();
-    var kwh = document.getElementById("kwh").value;
-    var nutz = document.getElementById("nutz").value;
-    var zeile = document.createElement("tr");
-    var datumzelle = document.createElement("td");
-    var kwhzelle = document.createElement("td");
-    var nutzzelle = document.createElement("td");
-    zeile.appendChild(datumzelle);
-    zeile.appendChild(nutzzelle);
-    zeile.appendChild(kwhzelle);
-    document.getElementById("ablesen").appendChild(zeile);
-    kwhzelle.textContent = kwh
-    datumzelle.textContent = datum
-    nutzzelle.textContent = nutz
-}
+    $("#btnAblesen").click(function(){
+      var val = $("input[name=nutzerkennung]").val();
+      if (validateInput(val)) {
+          $('#tableAblesen').append('<tr><td>' + $("input[name=nutzerkennung]").val() + '</td><td>' + $("input[name=verbrauchswert]").val() + '</td><td>' + heute +'</td></tr>');
+      } else {
+        $("#btnAblesen").after('<p id="fehlermeldung">' + "Falsche Nutzerkennung! Die Nutzerkennung muss aus den Initialen Ihres Namens und den letzten beiden Ziffern ihrer Matrikelnummer bestehen" + '</p>')
+        console.log();
+      }
+    });
+
+});
