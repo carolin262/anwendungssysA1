c7b2abd42eb06c3b554e66f076c404707bf22069
TheoVitkovskiy <fvitkovski@mail.de>
2017-05-25 11:50:02 +0200

    Some visual changes + added the toFixed(1) method

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
+
 var minNumber = 220;
 var maxNumber = 240;
 
 var datum = new Date();
-var heute = datum.getDate()+ "." + (datum.getMonth()+ 1)+ "." + datum.getFullYear();
+var heute = datum.getDate() + "." + (datum.getMonth() + 1) + "." + datum.getFullYear();
 
 var spannung = randomNumberFromRange(minNumber, maxNumber);
 var strom = randomNumberFromRange(0, parseInt(document.getElementById("maxstrom").innerHTML) + 5);
 
 
-function randomNumberFromRange(min,max)
-{
+function randomNumberFromRange(min,max) {
     return (Math.random()*(max-min+1)+min).toFixed(1);
 }
 
@@ -21,15 +21,19 @@ function validateInput(val) {
 $( document ).ready(function() {
 		$("#spannung").html(spannung);
 		$("#strom").html(strom);
+    
     if(strom > parseInt(document.getElementById("maxstrom").innerHTML)){
       $("#warnung").css("display", "block");
     }
+    
     $("#btnAblesen").click(function(){
       var val = $("input[name=nutzerkennung]").val();
       if (validateInput(val)) {
-          $('#tableAblesen').append('<tr><td>' + $("input[name=nutzerkennung]").val() + '</td><td>' + $("input[name=verbrauchswert]").val() + '</td><td>' + heute +'</td></tr>');
+          $('#tableAblesen').append('<tr><td>' + $("input[name=nutzerkennung]").val() + '</td><td>' + 
+          $("input[name=verbrauchswert]").val() + '</td><td>' + heute +'</td></tr>');
       } else {
-        $("#btnAblesen").after('<p id="fehlermeldung">' + "Falsche Nutzerkennung! Die Nutzerkennung muss aus den Initialen Ihres Namens und den letzten beiden Ziffern Ihrer Matrikelnummer bestehen" + '</p>')
+        $("#btnAblesen").after('<p id="fehlermeldung">' + "Falsche Nutzerkennung! Die Nutzerkennung muss"
+        + " aus den Initialen Ihres Namens und den letzten beiden Ziffern ihrer Matrikelnummer bestehen" + '</p>')
         console.log();
       }
     });
